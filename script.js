b2421d6e1bfa5d6782228825d5917c985b940348
Maddy2055 <mdhakal2@twu.edu>
2025-08-07 03:55:34 +0545

    All sections added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll('.nav-menu .nav-link');
 const menuOpenButton = document.querySelector('#menu-open-button');
 const menuCloseButton = document.querySelector('#menu-close-button');
 
@@ -5,4 +6,46 @@ menuOpenButton.addEventListener('click', () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
+// Close the menu when a nav link is clicked
 menuCloseButton.addEventListener('click', () => menuOpenButton.click());
+
+// Close the menu when a nav link is clicked
+// This ensure that clicking on any nav link will close the mobile menu
+navLinks.forEach(link => {
+    link.addEventListener('click', () => menuOpenButton.click());
+});
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+
+
+});
